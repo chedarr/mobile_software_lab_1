a5486f502ba16f631966943a5da58fe1d1d7b623
chedarr <dariyachebotar@yandex.ru>
2026-09-22 02:06:58 +0700

    Finish ComposeDemo

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -34,8 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DemoScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
